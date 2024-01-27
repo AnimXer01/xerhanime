cfd9dea8625f153d47fd0cf05fc2b0d221b7ca15
AnimXer01 <157260909+AnimXer01@users.noreply.github.com>
2024-01-28 00:49:29 +0800

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,7 +8,7 @@ hidres.style.display = "none";
 document.getElementById("search").value = q;
 
 $.ajax({
-    url: `https://hanipa.vercel.app/search`,
+    url: `https://hanime.tv/api/hanime/search`,
     data: JSON.stringify({
         "search": q,
         "tags": [],
