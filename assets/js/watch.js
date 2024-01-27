804bd21295a9f62fe924b03afa8ae8f93ddb0780
AnimXer01 <157260909+AnimXer01@users.noreply.github.com>
2024-01-28 01:16:47 +0800

    Update watch.js

--- assets/js/watch.js
+++ assets/js/watch.js
@@ -20,7 +20,7 @@ const tlt = document.querySelector('title');
 
 $(document).ready(function () {
     $.ajax({
-            url: `https://hanime.tv/api/hanime/getVideo/player?id=${id}`,
+            url: `https://hani.nsdev.ml/getVideo/player?id=${id}`,
             type: "GET",
             dataType: "json",
             success: async function (data) {
@@ -29,7 +29,7 @@ $(document).ready(function () {
             }
         })
     $.ajax({
-        url: `https://hanime.tv/api/hentai/getInfo?id=${id}`,
+        url: `https://hani.nsdev.ml/getInfo?id=${id}`,
         type: "GET",
         dataType: "json",
         success: async function (data) {
