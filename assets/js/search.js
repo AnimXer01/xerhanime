6783651ddd311aa4c7653394eb2d6fb966979911
AnimXer01 <157260909+AnimXer01@users.noreply.github.com>
2024-01-27 22:50:28 +0800

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,7 +8,7 @@ hidres.style.display = "none";
 document.getElementById("search").value = q;
 
 $.ajax({
-    url: `https://senpai-api.vercel.app/search`,
+    url: `https://hanipa.vercel.app/search`,
     data: JSON.stringify({
         "search": q,
         "tags": [],
